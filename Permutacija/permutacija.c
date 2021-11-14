53fa17c895aba40fbcbbb785fde976c591579906
Miha <mihavoncina.mv@gmail.com>
2021-11-14 21:52:55 +0100

    2.commit

--- Permutacija/permutacija.c
+++ Permutacija/permutacija.c
@@ -3,6 +3,21 @@
 
 int main(void){
 
+	int n, t;
+	char vrstica[] = "ABC";
+	n = strlen(vrstica);
+
+      	int  k = 0;
+	while(n - k > 1){
+	t = vrstica[k];
+	for(int i = k; i < n - 1; i++){
+		vrstica[i] = vrstica[i + 1];
+		}
+	vrstica[n - 1] = t;
+	printf("%s\n", vrstica);
+	k++;
+	}
+	
 	
 
 return 0;}
